df4201ebda1a2c5e55a9139d9f558da82ab19686
HP <nishtha.trivedi0604@gmail.com>
2023-05-06 02:09:01 +0530

    final commit

--- app/src/main/java/com/example/mflow/Signup.java
+++ app/src/main/java/com/example/mflow/Signup.java
@@ -9,5 +9,6 @@ public class Signup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        getSupportActionBar().setTitle(Signup);
     }
 }
